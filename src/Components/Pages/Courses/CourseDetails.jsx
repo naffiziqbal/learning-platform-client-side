0cad9027fd00706ae4f339fe9c341a702b936716
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-30 00:26:10 +0600

    React PDF

--- src/Components/Pages/Courses/CourseDetails.jsx
+++ src/Components/Pages/Courses/CourseDetails.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { createRef } from "react";
 import { useLoaderData } from "react-router-dom";
+import Pdf from "react-to-pdf";
 
 const CourseDetails = () => {
   const data = useLoaderData();
@@ -7,24 +8,28 @@ const CourseDetails = () => {
   const { title, course_Img, description, author, price, ratings, tagline } =
     data;
 
+  const ref = createRef();
   return (
     <div>
-      <p>Course Description</p>
+      <p className="text-3xl font-bold text-center">Course Description</p>
 
-      <div>
+      <div ref={ref}>
         <div className="card card-compact bg-base-100 shadow-xl">
           <figure>
-            <img src= {course_Img}  alt="Shoes" />
+            <img src={course_Img} alt="Shoes" />
           </figure>
           <div className="card-body">
             <h2 className="card-title">Name : {title}</h2>
             <p>{description}</p>
             <p>Author: {author}</p>
             <p>Price : {price}</p>
-            <p>Ratings :  {ratings}</p>
+            <p>Ratings : {ratings}</p>
           </div>
         </div>
       </div>
+      <Pdf targetRef={ref} filename="coursedetails.pdf">
+        {({ toPdf }) => <button className="btn btn-primary w-full" onClick={toPdf}>Download Details</button>}
+      </Pdf>
     </div>
   );
 };
