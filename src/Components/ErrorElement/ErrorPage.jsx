5d5eb030a8adc629d9323261ca0ebd7a02372a44
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-30 20:06:13 +0600

    Error Page Customization

--- src/Components/ErrorElement/ErrorPage.jsx
+++ src/Components/ErrorElement/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from './erros.svg'
 
 const ErrorPage = () => {
@@ -8,7 +9,9 @@ const ErrorPage = () => {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Ooops! Looks Like We're Lost</h1>
+      <h1 className="mb-5 text-5xl font-bold">Ooops! Looks Like We're Lost
+      </h1>
+      <p className=' link text-violet-700'><Link to ='/'>Let's get back to where we belong</Link></p>
       <p className="mb-5"></p>
     </div>
   </div>
